9a28ff8de32283cdbbd09757b6b37538215ddbb1
chaos430 <1192318530@qq.com>
2021-05-13 17:46:19 +0800

    初始化aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,7 +3,26 @@
     <topnav/>
   
       <div class="content">
-        <aside>边栏</aside>
+        <aside>
+          <h2>组件列表</h2>
+          <ol>
+            <li>
+              <router-link to="/doc/switch"></router-link>
+            </li>
+            <li>
+              <router-link to="/doc/button"></router-link>
+            </li>
+            <li>
+              <router-link to="/doc/dialog"></router-link>
+            </li>
+            <li>
+              <router-link to="/doc/tabs"></router-link>
+            </li>
+          </ol>
+
+
+
+        </aside>
         <main>主内容</main>
       </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,8 +12,8 @@
           <h1>柠檬UI</h1>
           <h2>一个厉害的 UI 框架</h2>
           <p class="actions">
-             <a href="">GitHub</a> 
-             <a href="">开始</a>   
+             <a href="https://github.com">GitHub</a> 
+             <router-link to="/doc">开始</router-link>   
           </p>
       </div>
   </div>
@@ -39,6 +39,27 @@ export default {
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+}
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
   }
 }
