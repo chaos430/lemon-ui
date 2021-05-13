bcf8b9ae299bc99abd79a184a83322fce34565f3
chaos430 <1192318530@qq.com>
2021-05-13 14:25:59 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>hi</div>
+ <jun/>
+</template>
+
+<script>
+import Jun from './components/Jun.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Jun
+  }
+}
+</script>
