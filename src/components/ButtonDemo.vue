48594c88991ad9854c271a899ff2eebaf70e5e31
chaos430 <1192318530@qq.com>
2021-05-16 19:50:11 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,11 +3,25 @@
         button示例
     </div>
     <h1>示例1</h1>
-    <div><button>你好</button></div>
+    <div>
+        <button>你好</button>
+        <button theme="button">你好</button>
+        <button theme="link">你好</button>
+        <button theme="text">你好</button>
+
+
+    </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-    components:{Button}
+    components:{Button},
+    setup(){
+        const onClick = ()=>{
+            console.log('hi');
+            
+        }
+        return {onClick}
+    }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,19 @@
 <template>
-    <button>
+<div>
+    <button class="lemon-button"
+     :class="`theme-${theme}`">
         <slot/>
     </button>
+</div>
 </template>
+<script lang="ts">
+
+export default {
+   props:{
+       theme:{
+           type: String,
+           default: 'button'
+       } 
+   }
+}
+</script>
