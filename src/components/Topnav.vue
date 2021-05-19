eff2000c96377e08dcfa1ce5efeaeb702d8077d7
chaos430 <1192318530@qq.com>
2021-05-19 21:55:49 +0800

    添加首页导航

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,9 +1,9 @@
 <template>
   
       <div class="topnav">
-        <div class="logo"><svg class="icon">
+        <router-link to="/" class="logo"><svg class="icon">
     <use xlink:href="#icon-lemon"></use>
-</svg></div>
+</svg></router-link>
       <ul class="menu">
         <li><router-link to="/doc">文档</router-link></li>
         
